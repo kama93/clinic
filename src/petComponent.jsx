b89f2c68afde07597de1c3fe70d687ce9cbec272
kamila <kamila@Pawels-MacBook-Pro.local>
2021-06-28 14:52:14 +0100

    change pet form

--- src/petComponent.jsx
+++ src/petComponent.jsx
@@ -1,26 +1,36 @@
+import './petComponent.css';
 
 function PetComponent() {
 
     return (
         <div>
             <form>
-                <label>
-                    Is going:
-                    <input name="isGoing"
-                        type="checkbox"
-                        // checked={}
-                        // onChange={} 
-                        />
-                </label>
+                <label for="lname">Last name: </label>
+                <input type="text" id="fsurname" name="fsurname" /><br />
+                <label for="fname">Pet name: </label>
+                <input type="text" id="pname" name="pname" /><br />
+                <label for="species">Species: </label>
+                <select id="species" name="species">
+                    <option value="dog">Dog</option>
+                    <option value="cat">Cat</option>
+                    <option value="hamster">Hamster</option>
+                    <option value="guinea_pig">Guinea Pig</option>
+                    <option value="rat">Rat</option>
+                    <option value="parrot">Parrot</option>
+                </select><br />
+                <label for="breed">Breed: </label>
+                <input type="text" id="breed" name="breed" /><br />
+                <input type="radio" id="female" name="fav_language" value="female" />
+                <label for="female">female</label>
+                <input type="radio" id="male" name="fav_language" value="male" />
+                <label for="male">male</label><br />
+                <label>Years: </label><input name="numberOfGuests" type="number" // value={} // onChange={} 
+                /> <br />
+                <label>Spayed/castrated: </label>
+                <input name="isGoing" type="checkbox" // checked={} // onChange={} 
+                />
                 <br />
-                <label>
-                    Number of guests:
-                        <input name="numberOfGuests"
-                        type="number"
-                        // value={}
-                        // onChange={} 
-                        />
-                </label>
+                <input type="submit" value="Submit" className='submit-buton-pet' />
             </form>
         </div>
     );
