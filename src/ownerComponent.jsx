ff73d5782f15c90fe41f408aa626814c0f1354cc
kamila <kamila@Pawels-MacBook-Pro.local>
2021-06-28 15:38:34 +0100

    add extra inputs to owner form

--- src/ownerComponent.jsx
+++ src/ownerComponent.jsx
@@ -7,11 +7,16 @@ function OwnerComponent() {
             <form>
                 <label for="lname">Last name: </label>
                 <input type="text" id="fsurname" name="fsurname" /><br />
-                <label for="fname">Name: </label>
+                <label for="fname">First name: </label>
                 <input type="text" id="name" name="name" /><br />
                 <label for="adress">Adress: </label>
                 <input type="text" id="adress" name="adress" /><br />
-                <br/>
+                <label for="email">Email: </label>
+                <input type="email"/><br/>
+                <label for="phone">Phone number: </label>
+                <input type="tel"/><br/>
+                <label for="phone">Second number: </label>
+                <input type="tel"/>
                 <div className='button-container'>
                 <input type="submit" value="Submit" className='submit-buton-pet' />
                 </div>
